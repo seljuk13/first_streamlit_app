ac7ada6ca9f0dbd41fee51ecd7148b0337d17c56
seljuk13 <142267205+seljuk13@users.noreply.github.com>
2023-08-14 14:45:12 -0700

    Update streamlit_app.py
    removed extra emojis and added fruit smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-boiled free range egg')
 streamlit.text('🥑🍞Avocado Toast')
-🥣 🥗 🐔 🥑🍞
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
